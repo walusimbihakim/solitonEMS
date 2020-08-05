79ee4c213c12252b092a7131076e125f5316516a
Bright Kanyange <brightkanyange@outlook.com>
2020-08-05 16:15:18 +0300

    Done with termination

--- contracts/admin.py
+++ contracts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from contracts.models import Contract, Penalty, Offence
+from contracts.models import Contract, Penalty, Offence, Termination
 
 admin.site.register(Contract)
 admin.site.register(Penalty)
 admin.site.register(Offence)
+admin.site.register(Termination)
